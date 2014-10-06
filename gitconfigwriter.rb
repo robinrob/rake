05e3d89a2f6f770da9e1323471458d9c3a25f782
Robin Smith <robin@mrrobinsmith.com>
2014-10-06 13:34:52 -0700

    Auto-update.

--- gitconfigwriter.rb
+++ gitconfigwriter.rb
@@ -4,7 +4,7 @@ $LOAD_PATH << '.'
 class GitConfigWriter
 
   def write(blocks, filename='.gitconfig')
-    File.open(filename, File::WRONLY | File::CREAT) do |file|
+    File.open(filename, File::WRONLY) do |file|
       blocks.each do |block|
         file.write(block.to_s)
       end
