34c98acde849c7799ecbec7d92b8e89e40aa8172
Robin Smith <robin@mrrobinsmith.com>
2014-10-10 13:27:37 -0700

    Auto-update.

---
+++ gitconfigfilecollection.rb
@@ -0,0 +1,15 @@
+$LOAD_PATH << '.'
+
+require 'test/unit'
+require 'gitconfigblock'
+require 'gitconfigblockbuilder'
+
+
+class TestGitConfigBlock < Test::Unit::TestCase
+
+  def test_should_sort_collection
+
+  end
+
+
+end
---
+++ test_gitconfigfilecollection.rb
@@ -0,0 +1,9 @@
+class GitConfigBlockCollection
+
+  attr_accessor :blocks
+
+  def initialize(blocks)
+    @blocks = blocks
+  end
+
+end
